2d9a554f44bf06878232791653acea1471930d24
Neetu Seharawat <neetumorbca@gmail.com>
2023-02-15 15:05:10 +0530

    Building Blocks of Ecommerce

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import {Fragment} from "react";
+import Items from "./Component/Items";
+import Header from "./Component/Header";
+
+export const productsArr = [
+  {
+    title: "Colors",
+    price: 100,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+  },
+ {
+    title: "Black and white Colors",
+    price: 50,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+  },
+  {
+    title: "Yellow and Black Colors",
+    price: 70,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+  },
+  {
+    title: "Blue Color",
+    price: 100,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+  },
+];
+
+function App() {
+return (
+<Fragment>
+  
+      
+  
+      <Header />
+      
+      <Items products={productsArr} />
+</Fragment>
+  );
+}
+
+export default App;
