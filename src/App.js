dd80106cd667663f52fe5e2f05aa3dd8c8cae11b
Neetu Seharawat <neetumorbca@gmail.com>
2023-02-21 23:56:18 +0530

    Implement the About page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import Header from "./Component/Layout/Header";
-import Body from "./Component/Layout/Body";
 import Footer from "./Component/Layout/Footer";
 import CartProvider from "./Component/Store/CartProvider";
+import HOME from "./Pages/Home";
+import ABOUT from "./Pages/About";
+import STORE from "./Pages/STORE";
+
+
+import { Routes,Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <CartProvider>
+return (
+<CartProvider>
       <Header />
-      <Body />
-      <Footer />
-    </CartProvider>
+          <Routes>
+          <Route path="/home" element={<HOME/>} />
+          <Route path="/store" element={<STORE/>} />
+          <Route path="/about" element={<ABOUT/>} />
+          </Routes>
+
+        <Footer />
+ </CartProvider> 
   );
 };
 export default App;
