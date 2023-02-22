ef2042f1677bc43ba7b9a5c55ed0ea22d6ed9808
Neetu Seharawat <neetumorbca@gmail.com>
2023-02-23 01:33:11 +0530

    Making Api calls in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import Header from "./Component/Layout/Header";
-import Footer from "./Component/Layout/Footer";
-import CartProvider from "./Component/Store/CartProvider";
-import HOME from "./Pages/Home";
-import ABOUT from "./Pages/About";
-import STORE from "./Pages/STORE";
+import React,{Fragment, useState} from "react";
 
-import { Routes,Route} from 'react-router-dom';
+import MovieList from './MoviePages/MoviesList';
+import './App.css';
 
-function App() {
-return (
-<CartProvider>
-      <Header />
-      
-          <Routes>
-          <Route path="/home" element={<HOME/>} />
-          <Route path="/store" element={<STORE/>} />
-          <Route path="/about" element={<ABOUT/>} />
-          </Routes>
+function App(){
+    const [movies, setMovies]= useState([]);
 
-        <Footer />
- </CartProvider> 
-  );
-};
+    function fetchMoviesHandler(){
+    fetch('https://swapi.dev/api/films/')
+    .then((response)=>{
+        return response.json();
+     })
+     .then ((data)=>{
+        const transformedMovies = data.results.map((movieData)=>{
+        return{
+            id:movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate : movieData.release_Date
+        };
+     }) ;
+     setMovies(transformedMovies);
+});
+}
+return(
+<Fragment>
+    <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+    </section>
+    <section>
+        <MovieList movies ={movies} />
+    </section>
+</Fragment>
+);
+}
 export default App;
