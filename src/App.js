d1f81c65c0328f910191150192249303a1d083fd
Neetu Seharawat <neetumorbca@gmail.com>
2023-02-25 14:06:08 +0530

    Adding Contact US Page via REACT ROUTER

--- src/App.js
+++ src/App.js
@@ -1,84 +1,26 @@
-import React,{Fragment, useState, useEffect,useCallback } from "react";
-import AddMovie from "./MoviePages/AddMovie";
-import MovieList from './MoviePages/MoviesList';
-import './App.css';
-//import { json } from "react-router-dom";
+import Header from "./Component/Layout/Header";
+import Footer from "./Component/Layout/Footer";
+import CartProvider from "./Component/Store/CartProvider";
+import HOME from "./Pages/Home";
+import ABOUT from "./Pages/About";
+import STORE from "./Pages/STORE";
+import ContactUs from "./Pages/ContactUs";
+import {  Routes ,Route } from 'react-router-dom';
 
-function App(){
-  const [movies, setMovies]= useState([]);
-  const [isLoading, setIsLoading]= useState(false);
-  const [error,setError] =useState(null);
 
-  
-  const fetchMoviesHandler = useCallback(async ()=> {
-    setIsLoading(true);
-    setError(null);
-    try{
-        
-        const response = await fetch('https://ecommerce-81a9e-default-rtdb.firebaseio.com/movies.json')
-        if(!response.ok){
-           throw new Error('Something went wrong ...Retry!!');
-        }
-
-        const data = await response.json();
-          
-
-        const loadedMovies=[];
-        for(const key in data){
-            loadedMovies.push ({
-                id:key,
-                title: data[key].title,
-                openingText: data[key].openingText,
-                releaseDate: data[key].releaseDate,
-            });
-        }
-           
-        setMovies(loadedMovies);   
-    }catch(error){
-    setError(error.message);
-    }
-    setIsLoading(false);
-    },[]);
-
-    useEffect(()=>{
-        fetchMoviesHandler();
-         },[fetchMoviesHandler]);
-
-    async function addMoviesHandler(movie){
-       const response = await fetch('https://ecommerce-81a9e-default-rtdb.firebaseio.com/movies.json' , {
-            method:'POST',
-            body: JSON.stringify(movie),
-            headers:{
-                'Content-Type': 'application/json'
-            }       
-         });
-         const data = await response.json();
-         console.log(data);
-     }
- 
-    let content =<p>Found no Movie!</p>
-    if(movies.length >0){
-    content = <MovieList movies ={movies} />;
-    }
-    if (error){
-        content =<p>error</p>;
-    }
-    if(isLoading){
-        content = <p>Loading..</p>;
-    }
-
-return(
-<Fragment>
-    <section>
-        <AddMovie onAddMovie={addMoviesHandler}/>
-    </section>
-    <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-    </section>
-    <section>
-     {content}        
-    </section>
-</Fragment>
-);
-}
+function App() {
+return (
+<CartProvider>
+      <Header />
+          <Routes>
+          <Route path="/home" element={<HOME/>} />
+          <Route path="/store" element={<STORE/>} />
+          <Route path="/about" element={<ABOUT/>} />
+          <Route path="/contactUs" element={<ContactUs/>}/>
+          </Routes>
+         
+        <Footer />
+ </CartProvider> 
+  );
+};
 export default App;
